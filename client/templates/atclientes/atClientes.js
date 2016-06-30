18c3ee9e45693361be1e2ffcbef7045a80271a6a
Gustavo Falavigna <Gustavo Falavigna>
2016-06-29 21:10:50 -0300

    arreglado clientesSubscriptions

--- client/templates/atclientes/atClientes.js
+++ client/templates/atclientes/atClientes.js
@@ -1,10 +1,9 @@
 Template.searchAtClientesForm.events({
 	"submit form": function(e){
-			Meteor.subscribe('clientesPublish', e.target.apellido.value, e.target.nombres.value, e.target.documentoNro.value);
-			var cl = Clientes.find();
-			for (var i=0; i < cl.length(); i++) {
-				Meteor.subscribe("cuentasPublish", i.clienteId);
-			}
+		if (typeof clienteSubscript !== 'undefined') {
+    		clienteSubscript.stop();
+    	};
+		clienteSubscript = Meteor.subscribe('clientesPublish', e.target.apellido.value, e.target.nombres.value, e.target.documentoNro.value);
 	}
 
 })
--- client/templates/clientes/clientes.js
+++ client/templates/clientes/clientes.js
@@ -1,11 +1,15 @@
 Template.clientesNewTemplate.events({
 	"submit form": function(e){
+		if (clienteSubscrip) {clienteSubscrip.stop()};
+		clienteSubscrip = Meteor.subscribe("clientesPublish",null,null,e.target.documentoNro.value);
 		Router.go('clientesListTemplate');
 	}
 })
 
 Template.clientesShowTemplate.events({
 	"click button.btn.btn-info": function(e){
+		if (clienteSubscrip) {clienteSubscrip.stop()};
+		clienteSubscrip = Meteor.subscribe("clientesPublish",null,null,this.documentoNro);
 		Currents.insert({"entorno": "AtClientes", "tipo":"idCliente", "idCollection":this._id, "fLog": new Date, "usuario": Meteor.userId()})
 		Router.go('clientesListTemplate');
 	}
